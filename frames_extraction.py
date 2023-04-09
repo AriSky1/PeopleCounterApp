c4f89638b1eee61f63eb573167f785a2dbad7d88
Arina <105871709+AriSky1@users.noreply.github.com>
2023-04-10 01:48:19 +0200

    ''

--- frames_extraction.py
+++ frames_extraction.py
@@ -1,9 +1,14 @@
 import glob
 import cv2
+import os
+import time
+import re
 
+absolute_path = os.path.dirname(__file__)
+relative_path = "videos"
+d = os.path.join(absolute_path, relative_path)
 
-
-d = r"C:\Users\ariai\Documents\DATA SCIENCE\PROJECTS\PeopleCounterApp\videos"
+df = r"C:\Users\ariai\Documents\DATA SCIENCE\PROJECTS\PeopleCounterApp\frames"
 fl = glob.glob(d + "/*.mp4")
 counter = 0
 cam = cv2.VideoCapture(fl[counter])
@@ -16,6 +21,7 @@ while(True):
     ret,frame = cam.read()
     if ret:
         name = 'frames\Frame(' + str(currentframe) + ').jpg'
+
         cv2.imwrite(name, frame)
         currentframe += 1
     else:
@@ -23,3 +29,7 @@ while(True):
 
 cam.release()
 cv2.destroyAllWindows()
+
+
+images=glob.glob(df+"/*[0]*")
+print(images)
