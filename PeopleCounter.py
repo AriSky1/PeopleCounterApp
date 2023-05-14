46d5b248a263240d4af1e5be917a379dd484a269
Arina <105871709+AriSky1@users.noreply.github.com>
2023-05-15 00:45:39 +0200

    'fix'

--- PeopleCounter.py
+++ PeopleCounter.py
@@ -13,85 +13,25 @@ import cv2
 from flask import Flask, render_template, Response
 from datetime import datetime
 import pytz
+
 import time
 import imutils
-from gen_frames import gen_frames
+from gen_frames import gen_frames_yolo, gen_frames_hog
+import dash_bootstrap_components as dbc
 
+external_stylesheets = [
+    'https://fonts.googleapis.com/css2?family=Wix+Madefor+Display:wght@600&display=swap',
+     dbc.themes.BOOTSTRAP
+]
 
-style_flex={"display":"flex", "align-items":"flex-end", "gap":"20px",}
+style_flex={"display":"flex", "align-items":"flex-end", "gap":"20px", 'padding-left': '20px'}
 style_dd={'width':'200px','background-color': '#F9F8F9', 'height':'40px', 'font_family': 'Tahoma'}
 style_btn = {'background-color': '#7FFF00','color': 'black','font-weight': 'bold', 'width':'200px', 'height':'40px'}
-style_text={'color': 'grey','fontSize': 18,'textAlign': 'left','font_family': 'Segoe UI', 'padding-bottom':'20px'}
-style_title={'color': 'grey','fontSize': 30,'textAlign': 'left','font_family': 'Tahoma', 'letter-spacing':'2px'}
+style_text={'color': 'grey','fontSize': 18,'textAlign': 'left','font_family': 'Segoe UI', 'padding-bottom':'20px','padding-left': '20px'}
+style_title={'color': 'grey','fontSize': 30,'textAlign': 'left', 'letter-spacing':'2px', 'padding-left': '20px','padding-top': '20px'}
 
 server = Flask(__name__)
-app = Dash(__name__, server=server)
-
-
-# model = YOLO('yolov8n.pt')
-# # url = 'https://www.youtube.com/watch?v=cH7VBI4QQzA' #disctricts walking live
-# video = pafy.new(url)
-# best = video.getbest(preftype="mp4")
-# cap = cv2.VideoCapture(best.url)
-#
-# def gen_frames():
-#     prev_frame_time = 0
-#     new_frame_time = 0
-#
-#     while(cap.isOpened()):
-#         grabbed, frame = cap.read()
-#         frame = imutils.resize(frame, width=800)
-#         image = frame
-#         def zoom_at(img, zoom=1.2, angle=0, coord=None):
-#             cy, cx = [i / 2 for i in img.shape[:-1]] if coord is None else coord[::-1]
-#             rot_mat = cv2.getRotationMatrix2D((cx, cy), angle, zoom)
-#             result = cv2.warpAffine(img, rot_mat, img.shape[1::-1], flags=cv2.INTER_LINEAR)
-#             return result
-#         image=zoom_at(image)
-#         results = model.predict(image)
-#         results=results[0].boxes.boxes
-#         results = results.numpy()
-#         new_frame_time = time.time()
-#         fps = 1 / (new_frame_time - prev_frame_time)
-#         prev_frame_time = new_frame_time
-#         fps = int(fps)
-#         fps = str(fps)
-#         count = 0
-#         for res in results:
-#             # print(res)
-#             x1, y1, x2, y2, score,label=int(res[0]),int(res[1]),int(res[2]),int(res[3]),int(res[4]*100),int(res[5])
-#             names = model.model.names
-#             if label in names:
-#                 label = names[label]
-#                 if label == 'person':
-#                     cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 1)
-#                     # cv2.putText(image, f'{label}', (x1, y1-20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-#                     cv2.putText(image, f'{score} %', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
-#                     count += 1
-#
-#
-#         cv2.putText(img=image, text=str(count), org = (950, 160),fontFace = cv2.FONT_HERSHEY_DUPLEX, fontScale = 5.0,color=(125, 246, 55),thickness = 9)
-#         cv2.putText(img=image, text="YOLOv8", org=(300, 30), fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1.2,color=(125, 246, 55), thickness=2)
-#         cv2.putText(image, str(datetime.now(tz=pytz.timezone('Asia/Tokyo')).strftime("%Y-%m-%d %H:%M:%S")), (900, 30), cv2.FONT_HERSHEY_DUPLEX, 1, (125, 246, 55), 2,cv2.LINE_AA)
-#         cv2.putText(img=image, text=(str(fps)+' fps'), org=(700, 30), fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1.0,color=(125, 246, 55), thickness=2)
-#
-#         frame = cv2.imencode('.jpg', image)[1].tobytes()
-#         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-#         key = cv2.waitKey(20)
-#         if key == 27:
-#             break
-
-
-
-
-
-
-
-
-
-
-
-
+app = Dash(__name__, server=server,external_stylesheets=external_stylesheets)
 
 
 app.layout = html.Div(children=[
@@ -106,7 +46,7 @@ html.Div(children='''
 
              style=style_flex),
     html.Br(),
-    html.Div(id='container'),
+    html.Div(id='container', style={'padding-left': '20px'}),
 
 
     # html.Img(id='stream',src="/video_feed"),
@@ -117,28 +57,52 @@ html.Div(children='''
 ])
 
 # yolo8 + Shibuya static
-@server.route('/video_feed')
-def video_feed():
+@server.route('/yolo8_1')
+def yolo8_1():
     url = 'https://www.youtube.com/watch?v=IBFCV4zhMGc' #shibuya static
     model = YOLO('yolov8n.pt')
-    return Response(gen_frames(url, model),
+    return Response(gen_frames_yolo(url, model),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # yolo8 + Street walk
-@server.route('/video_feed2')
-def video_feed2():
+@server.route('/yolo8_2')
+def yolo8_2():
     url = 'https://www.youtube.com/watch?v=cH7VBI4QQzA'  # street walk
     model = YOLO('yolov8n.pt')
-    return Response(gen_frames(url, model),
+    return Response(gen_frames_yolo(url, model),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # yolo8 + Street static
-@server.route('/video_feed3')
-def video_feed3():
+@server.route('/yolo8_3')
+def yolo8_3():
     url = 'https://www.youtube.com/watch?v=3kPH7kTphnE'  # street static
     model = YOLO('yolov8n.pt')
-    return Response(gen_frames(url, model),
+    return Response(gen_frames_yolo(url, model),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+# HOG + Shibuya static
+@server.route('/hog_1')
+def hog_1():
+    url = 'https://www.youtube.com/watch?v=IBFCV4zhMGc' #shibuya static
+    return Response(gen_frames_hog(url),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# HOG + Street walk
+@server.route('/hog_2')
+def hog_2():
+    url = 'https://www.youtube.com/watch?v=cH7VBI4QQzA' # street walk
+    return Response(gen_frames_hog(url),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+# HOG + Street static
+@server.route('/hog_3')
+def hog_3():
+    url = 'https://www.youtube.com/watch?v=3kPH7kTphnE' # street static
+    return Response(gen_frames_hog(url),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 # CALLBACKS
 
@@ -161,11 +125,17 @@ def display_stream(n_clicks,cvmodel, video):
     #     url = 'https://www.youtube.com/watch?v=cH7VBI4QQzA' #disctricts walking live
 
     if n_clicks > 0 and cvmodel == 'Yolo8' and video == 'Shibuya static':
-        return html.Div([html.Img(id='stream', src="/video_feed")])
+        return html.Div([html.Img(id='stream', src="/yolo8_1")])
     if n_clicks > 0 and cvmodel == 'Yolo8' and video == 'Street walk':
-        return html.Div([html.Img(id='stream', src="/video_feed2")])
+        return html.Div([html.Img(id='stream', src="/yolo8_2")])
     if n_clicks > 0 and cvmodel == 'Yolo8' and video == 'Street static':
-        return html.Div([html.Img(id='stream', src="/video_feed3")])
+        return html.Div([html.Img(id='stream', src="/yolo8_3")])
+    if n_clicks > 0 and cvmodel == 'HOG' and video == 'Shibuya static':
+        return html.Div([html.Img(id='stream', src="/hog_1")])
+    if n_clicks > 0 and cvmodel == 'HOG' and video == 'Street walk':
+        return html.Div([html.Img(id='stream', src="/hog_2")])
+    if n_clicks > 0 and cvmodel == 'HOG' and video == 'Street static':
+        return html.Div([html.Img(id='stream', src="/hog_3")])
 
 
 
--- gen_frames.py
+++ gen_frames.py
@@ -15,9 +15,11 @@ from datetime import datetime
 import pytz
 import time
 import imutils
+import numpy as np
+from imutils.object_detection import non_max_suppression
 
 
-def gen_frames(url, model):
+def gen_frames_yolo(url, model):
     video = pafy.new(url)
     best = video.getbest(preftype="mp4")
     cap = cv2.VideoCapture(best.url)
@@ -68,6 +70,59 @@ def gen_frames(url, model):
                     color=(125, 246, 55), thickness=2)
 
         frame = cv2.imencode('.jpg', image)[1].tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        key = cv2.waitKey(20)
+        if key == 27:
+            break
+
+
+def gen_frames_hog(url):
+    video = pafy.new(url)
+    best = video.getbest(preftype="mp4")
+    cap = cv2.VideoCapture(best.url)
+
+    # HOG descriptor/person detector
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    prev_frame_time = 0
+    new_frame_time = 0
+    while (cap.isOpened()):
+        ret, frame = cap.read()  # import image
+        frame = imutils.resize(frame, width=800)
+        image = cv2.resize(frame, (0, 0), None, 1, 1)
+        frame = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # converts image to gray
+
+        # pedestrians, weights = hog.detectMultiScale(frame, winStride=(4,4),padding=(4, 4), scale=1.5)
+        pedestrians, weights = hog.detectMultiScale(frame)
+        pedestrians = np.array([[x, y, x + w, y + h] for (x, y, w, h) in pedestrians])
+
+        count = 0
+        pedestrians = non_max_suppression(pedestrians, probs=None, overlapThresh=0.8)
+        for x, y, w, h in pedestrians:
+            cv2.rectangle(image, (x, y), (w, h), (0, 0, 100), 2)
+            cv2.rectangle(image, (x, y - 20), (w, y), (0, 0, 255), -1)
+            cv2.putText(image, f'P{count}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+            count += 1
+
+        new_frame_time = time.time()
+        fps = 1 / (new_frame_time - prev_frame_time)
+        prev_frame_time = new_frame_time
+        fps = int(fps)
+        fps = str(fps)
+        cv2.putText(img=image, text=str(count), org=(600, 160), fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=4.0,
+                    color=(125, 246, 55), thickness=7)
+        cv2.putText(img=image, text=str(count), org=(950, 160), fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=5.0,
+                    color=(125, 246, 55), thickness=9)
+        # cv2.putText(img=image, text="Detection model : HOG", org=(20, 30), fontFace=cv2.FONT_HERSHEY_DUPLEX,
+        #             fontScale=1.2, color=(125, 246, 55), thickness=3)
+        # cv2.putText(image, str(datetime.now(tz=pytz.timezone('Asia/Tokyo')).strftime("%Y-%m-%d %H:%M:%S")),
+        #             (900, 30), cv2.FONT_HERSHEY_DUPLEX, 1, (125, 246, 55), 2, cv2.LINE_AA)
+        cv2.putText(img=image, text=(str(fps) + ' fps'), org=(700, 30), fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1.0,
+                    color=(125, 246, 55), thickness=2)
+
+        frame = cv2.imencode('.jpg', image)[1].tobytes()
+
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         key = cv2.waitKey(20)
         if key == 27:
